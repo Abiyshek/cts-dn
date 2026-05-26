25b73eb82291c3bd8a164e56cfe4f431e22aaa24
Abiyshek <aabiyshek@gmail.com@github.com>
2026-05-26 11:47:17 +0530

    navigation.html

---
+++ server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  // Default to index.html or base_template.html
+  let filePath = req.url === '/' ? '/Module_1/HTML/base_template.html' : req.url;
+  filePath = path.join(__dirname, filePath);
+
+  // Read and serve the file
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>404 - File Not Found</h1>');
+      } else {
+        res.writeHead(500);
+        res.end('Server Error');
+      }
+    } else {
+      // Determine content type
+      const ext = path.extname(filePath);
+      let contentType = 'text/html';
+      if (ext === '.css') contentType = 'text/css';
+      if (ext === '.js') contentType = 'text/javascript';
+      
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
